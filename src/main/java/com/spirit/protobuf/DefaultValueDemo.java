e1e5c930fafba6b39dedba6b1cce2c3409341740
VladislavTitarenko <vladislav@1worldonline.com>
2023-10-22 21:04:37 +0300

    Default value demo

---
+++ src/main/java/com/spirit/protobuf/DefaultValueDemo.java
@@ -0,0 +1,10 @@
+package com.spirit.protobuf;
+
+import com.spirit.models.Person;
+
+public class DefaultValueDemo {
+    public static void main(String[] args) {
+        Person person = Person.newBuilder().build();
+        System.out.println("City : " + person.getAddress().getCity());
+    }
+}
